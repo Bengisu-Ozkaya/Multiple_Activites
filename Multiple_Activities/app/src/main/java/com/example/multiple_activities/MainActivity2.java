4df7b3cac8b3eec8936331927a67dd599dc1058a
bngs <162094351+Bengisu-Ozkaya@users.noreply.github.com>
2025-06-30 00:49:36 +0300

    Multiple_Activities

---
+++ Multiple_Activities/app/src/main/java/com/example/multiple_activities/MainActivity2.java
@@ -0,0 +1,38 @@
+package com.example.multiple_activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textView = findViewById(R.id.textView);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("nameInput");
+        textView.setText(name);
+    }
+
+    public void change2(View view){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+}
